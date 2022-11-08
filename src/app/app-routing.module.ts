862c9dc5e1248a0643af31b8dbdf904bb357ea6d
damian <damiant12345@gmail.com>
2022-11-08 19:28:05 +0100

    feat: implemented product form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ProductFormComponent } from './components/product-form/product-form.component';
+import { ProductFormComponentModule } from './components/product-form/product-form.component-module';
+import { ProductsServiceModule } from './services/products.service-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([])],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot([
+      { path: 'create-product', component: ProductFormComponent },
+    ]),
+    ProductFormComponentModule,
+    ProductsServiceModule,
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
