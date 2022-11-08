7d76f09b10b85186556517e45a6da0d853f2892d
damian <damiant12345@gmail.com>
2022-11-08 19:58:32 +0100

    feat: implemented employee form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductFormComponent } from './components/product-form/product-form.component';
+import { EmployeeFormComponent } from './components/employee-form/employee-form.component';
 import { ProductFormComponentModule } from './components/product-form/product-form.component-module';
 import { ProductsServiceModule } from './services/products.service-module';
+import { EmployeeFormComponentModule } from './components/employee-form/employee-form.component-module';
+import { EmployeesServiceModule } from './services/employees.service-module';
 
 @NgModule({
   imports: [
     RouterModule.forRoot([
       { path: 'create-product', component: ProductFormComponent },
+      { path: 'create-employee', component: EmployeeFormComponent }
     ]),
     ProductFormComponentModule,
     ProductsServiceModule,
+    EmployeeFormComponentModule,
+    EmployeesServiceModule,
   ],
   exports: [RouterModule],
 })
---
+++ src/app/services/employees.service-module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { EmployeesService } from './employees.service';
+
+@NgModule({
+  imports: [],
+  declarations: [],
+  providers: [EmployeesService],
+  exports: [],
+})
+export class EmployeesServiceModule {}
